42c93ea8e2b5ae96c354c2da9d46da4101e9d2ad
Yusuke Miyazaki <ysk1180@gmail.com>
2022-10-11 16:06:23 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+// 参考: https://zenn.dev/kowaremonoid/articles/7e077f9eb4439b
+
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root));
+
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
