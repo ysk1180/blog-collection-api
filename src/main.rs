0db5c95c80370797e02e3647d494f5cb547c8710
Yusuke Miyazaki <ysk1180@gmail.com>
2022-10-18 15:51:01 +0900

    RSSのフィードを取得できるようにした

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 // 参考: https://zenn.dev/kowaremonoid/articles/7e077f9eb4439b
 
 use axum::{routing::get, Router};
+use rss::Channel;
+use serde::Serialize;
 use std::net::SocketAddr;
 
 #[tokio::main]
@@ -23,6 +25,30 @@ async fn main() {
         .unwrap();
 }
 
+#[derive(Serialize, Debug)]
+struct FeedItem {
+    title: String,
+    link: String,
+}
+
+// 参考：https://blog-dry.com/entry/2018/03/21/225533
+fn fetch_feed(url: &str) -> Vec<FeedItem> {
+    let channel = Channel::from_url(url).unwrap();
+    let items: Vec<FeedItem> = channel
+        .items()
+        .iter()
+        .map(|item| FeedItem {
+            title: item.title().unwrap().to_string(),
+            link: item.link().unwrap().to_string(),
+        })
+        .collect();
+    items
+}
+
 async fn root() -> &'static str {
+    let url = "https://ysk-pro.hatenablog.com/rss";
+    let items = fetch_feed(url);
+    items.iter()
+        .for_each(|item| println!("{}", serde_json::to_string(&item).unwrap()));
     "Hello, World!"
 }
